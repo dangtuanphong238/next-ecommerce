9c11cf992c2e022c84370cac513f0e68f243b48f
Đặng Tuấn Phong <Phongstork@gmail.com>
2023-05-15 08:48:39 +0700

    config page

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}  basePath='/next-ecommerce-admin-7gkw.vercel.app/api/auth'>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
