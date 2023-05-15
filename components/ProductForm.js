a02ddc875838df007660661b17ebe4eab2493428
Đặng Tuấn Phong <Phongstork@gmail.com>
2023-05-15 16:19:26 +0700

    update key

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -58,7 +58,7 @@ export default function ProductForm({
 
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('upload_preset', `${process.env.UPLOAD_PRESET_KEY}`);
+    formData.append('upload_preset', 'h47tbvog');
 
     const data = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_NAME}/image/upload`, {
       method: 'POST',
