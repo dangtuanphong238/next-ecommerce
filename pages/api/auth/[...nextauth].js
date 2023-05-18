112ca1eb24f175d6e10e8915933926b9d9dbfac9
Đặng Tuấn Phong <Phongstork@gmail.com>
2023-05-18 09:58:30 +0700

    updatecode

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -20,25 +20,25 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  callbacks: {
-    session: ({ session, token, user }) => {
-      console.log({ session, token, user })
-      if (adminEmails.includes(session?.user?.email)) {
-        return session;
-      } else {
-        return false
-      }
-    },
-  },
+  // callbacks: {
+  //   session: ({ session, token, user }) => {
+  //     console.log({ session, token, user })
+  //     if (adminEmails.includes(session?.user?.email)) {
+  //       return session;
+  //     } else {
+  //       return false
+  //     }
+  //   },
+  // },
 }
 
 export default NextAuth(authOptions)
 
-export async function isAdminRequest(req, res) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!adminEmails.includes(session?.user?.email)) {
-    res.status(401);
-    res.end();
-    throw 'not an admin';
-  }
-}
+// export async function isAdminRequest(req, res) {
+//   const session = await getServerSession(req, res, authOptions);
+//   if (!adminEmails.includes(session?.user?.email)) {
+//     res.status(401);
+//     res.end();
+//     throw 'not an admin';
+//   }
+// }
